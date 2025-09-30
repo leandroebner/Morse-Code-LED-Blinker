b7715544869ac34c0002ae9321353b59ff9eb370
Leandro Ebner <leandro.ebner@outlook.de>
2025-09-30 18:58:28 +0200

    Cleaned up code snippet

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -5,8 +5,8 @@
 
 // Define the Morse code timing (in milliseconds)
 #define DOT_DURATION 200
-#define DASH_DURATION (DOT_DURATION * 3)
 #define SYMBOL_GAP DOT_DURATION
+#define DASH_DURATION (DOT_DURATION * 3)
 #define LETTER_GAP (DOT_DURATION * 3)
 #define WORD_GAP (DOT_DURATION * 7)
 
